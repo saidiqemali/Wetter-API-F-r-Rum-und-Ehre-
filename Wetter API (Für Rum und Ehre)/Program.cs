ac830b289133a0b2134bc566de81e6827899208c
Saidi Qemali <sajdiqemali@gmail.com>
2021-10-20 08:36:43 +0200

    Add project files.

---
+++ Wetter API (Für Rum und Ehre)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wetter_API__Für_Rum_und_Ehre_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
