95e0f4a3375e4b5a0d9e87100b546b7d4f0fa585
Saidi Qemali <sajdiqemali@gmail.com>
2021-10-20 09:35:49 +0200

    C# Code : Package installieren zum Json in C# umwandeln zu können

--- Wetter API (Für Rum und Ehre)/Program.cs
+++ Wetter API (Für Rum und Ehre)/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Wetter_API__Für_Rum_und_Ehre_
 {
@@ -12,14 +13,30 @@ namespace Wetter_API__Für_Rum_und_Ehre_
 
             HttpClient httpClient = new HttpClient();
 
-            string requestUri = "https://api.openweathermap.org/data/2.5/weather?q=dortmund&appid=14ef7fc4abca59f2800df70ab9c334f6&units=metric";
+            string requestUri = "https://api.openweathermap.org/data/2.5/weather?q=Buelach&appid=14ef7fc4abca59f2800df70ab9c334f6&units=metric";
 
             HttpResponseMessage httpResponse = httpClient.GetAsync(requestUri).Result;
 
+
+
             string response = httpResponse.Content.ReadAsStringAsync().Result;
+
+
+            weatherMapResponse weatherMapResponse = JsonConvert.DeserializeObject<weatherMapResponse>(response);
             Console.WriteLine(response);
+
 
             Console.ReadKey();
         }
+    }
+
+    class weatherMapResponse
+    {
+        public Main main;
+    }
+
+    class Main
+    {
+        public float temp;
     }
 }
