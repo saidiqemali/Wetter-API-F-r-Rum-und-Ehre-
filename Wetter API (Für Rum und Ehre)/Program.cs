5722fc96c3dac305e8428723b57d2d52c317543a
Saidi Qemali <sajdiqemali@gmail.com>
2021-10-20 09:43:21 +0200

    C# Code : Console.WriteLine Variable für gewuenschte Stadt gemacht

--- Wetter API (Für Rum und Ehre)/Program.cs
+++ Wetter API (Für Rum und Ehre)/Program.cs
@@ -11,9 +11,12 @@ namespace Wetter_API__Für_Rum_und_Ehre_
             //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
             //14ef7fc4abca59f2800df70ab9c334f6
 
+            Console.WriteLine("Stadt eintragen");
+            string city = Console.ReadLine();
+
             HttpClient httpClient = new HttpClient();
 
-            string requestUri = "https://api.openweathermap.org/data/2.5/weather?q=Buelach&appid=14ef7fc4abca59f2800df70ab9c334f6&units=metric";
+            string requestUri = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=14ef7fc4abca59f2800df70ab9c334f6&units=metric";
 
             HttpResponseMessage httpResponse = httpClient.GetAsync(requestUri).Result;
 
@@ -23,7 +26,8 @@ namespace Wetter_API__Für_Rum_und_Ehre_
 
 
             weatherMapResponse weatherMapResponse = JsonConvert.DeserializeObject<weatherMapResponse>(response);
-            Console.WriteLine(response);
+            Console.WriteLine(" In " + city + " ist " + weatherMapResponse.main.temp + " Grad");
+
 
 
             Console.ReadKey();
